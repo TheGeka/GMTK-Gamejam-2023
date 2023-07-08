446f1875c3a6e38076347b628614add83d062563
Rloundy <rlrjloundy@gmail.com>
2023-07-07 22:45:13 -0500

    Added Survivors Changed Colors

--- Units/Grunt/Grunt.cs
+++ Units/Grunt/Grunt.cs
@@ -1,9 +1,11 @@
-namespace GMTKGameJam2023;
-
-public partial class Grunt : Units.Units
+namespace GMTKGameJam2023
 {
-    protected override float HitPoints { get; set; } = 1;
-    protected override float CarryWeight { get; set; } = 1;
-    protected override float Reach { get; set; } = 1;
-    protected override float MovementSpeed { get; set; } = 10.0f;
+
+    public partial class Grunt : Units.Units
+    {
+        protected override float HitPoints { get; set; } = 1;
+        protected override float CarryWeight { get; set; } = 1;
+        protected override float Reach { get; set; } = 1;
+        protected override float MovementSpeed { get; set; } = 10.0f;
+    }
 }
---
+++ Units/Survivor/Survivor.cs
@@ -0,0 +1,10 @@
+namespace GMTKGameJam2023
+{
+	public partial class Survivor : Units.Units
+	{
+		protected override float HitPoints { get; set; } = 1;
+		protected override float CarryWeight { get; set; } = 1;
+		protected override float Reach { get; set; } = 1;
+		protected override float MovementSpeed { get; set; } = 10.0f;
+	}
+}

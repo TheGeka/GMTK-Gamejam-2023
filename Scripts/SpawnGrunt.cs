4484aabe34b2cabaefcb02a0ebe8371d95f3c5e8
Rloundy <rlrjloundy@gmail.com>
2023-07-07 20:41:41 -0500

    Revert "Finished Survivor Demo"
    This reverts commit 9cbd3ca0e1a5fc71698f7389f8b31312fe7eed52.

--- Scripts/SpawnGrunt.cs
+++ Scripts/SpawnGrunt.cs
@@ -4,9 +4,8 @@ namespace GMTKGameJam2023.Scripts;
 
 public partial class SpawnGrunt : Node3D
 {
-	private PackedScene gruntscene;
+    private PackedScene gruntscene;
 
-<<<<<<< refs/remotes/origin/develop
     private void PlaceGrunt(Vector3 Location)
     {
         gruntscene = GD.Load<PackedScene>("res://Units/Grunt/grunt.tscn");
@@ -14,25 +13,16 @@ public partial class SpawnGrunt : Node3D
         newgrunt.Position = Location;
         AddChild(newgrunt);
     }
-=======
-	private void PlaceGrunt(Vector3 Location)
-	{
-		gruntscene = GD.Load<PackedScene>("res://grunt.tscn");
-		var newgrunt = gruntscene.Instantiate<Grunt>();
-		newgrunt.Position = Location;
-		AddChild(newgrunt);
-	}
->>>>>>> Finished Survivor Demo
 
-	public override void _Input(InputEvent @event)
-	{
-		base._Input(@event);
-		if (@event is InputEventMouseButton mouseButtonEvent)
-			if (mouseButtonEvent.ButtonIndex == MouseButton.Right && mouseButtonEvent.Pressed)
-			{
-				var camera = GetTree().Root.GetCamera3D();
-				var intersection = camera.CastRay(GetWorld3D().DirectSpaceState, mouseButtonEvent.Position);
-				PlaceGrunt(intersection["position"].AsVector3());
-			}
-	}
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (@event is InputEventMouseButton mouseButtonEvent)
+            if (mouseButtonEvent.ButtonIndex == MouseButton.Right && mouseButtonEvent.Pressed)
+            {
+                var camera = GetTree().Root.GetCamera3D();
+                var intersection = camera.CastRay(GetWorld3D().DirectSpaceState, mouseButtonEvent.Position);
+                PlaceGrunt(intersection["position"].AsVector3());
+            }
+    }
 }

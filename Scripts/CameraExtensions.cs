b47f78b8aa6c4399d556432a7f4cb5db90959bdc
Luna <Bracht5@live.com>
2023-07-08 03:30:09 +0200

    Code cleanup

--- Scripts/CameraExtensions.cs
+++ Scripts/CameraExtensions.cs
@@ -1,25 +1,20 @@
-using System.Collections.Generic;
 using Godot;
 using Godot.Collections;
 
-namespace GMTKGameJam2023
+namespace GMTKGameJam2023.Scripts;
+
+public static class CameraExtensions
 {
-    public static class CameraExtensions
+    public static Dictionary CastRay(this Camera3D camera, PhysicsDirectSpaceState3D spaceState, Vector2 mousePosition)
     {
-        public static Dictionary CastRay(this Camera3D camera, PhysicsDirectSpaceState3D spaceState, Vector2 mousePosition )
+        var rayOrigin = camera.ProjectRayOrigin(mousePosition);
+        var rayEnd = rayOrigin + camera.ProjectRayNormal(mousePosition) * 2000;
+        var intersection = spaceState.IntersectRay(new PhysicsRayQueryParameters3D()
         {
-            var rayOrigin = camera.ProjectRayOrigin(mousePosition);
-            var rayEnd = rayOrigin + camera.ProjectRayNormal(mousePosition) * 2000;
-            var intersection = spaceState.IntersectRay(new PhysicsRayQueryParameters3D()
-            {
-                From = rayOrigin,
-                To = rayEnd
-            });
+            From = rayOrigin,
+            To = rayEnd
+        });
 
-            return intersection;
-            
-
-            
-        }
+        return intersection;
     }
 }

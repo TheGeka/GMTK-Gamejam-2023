93f55e61c10805ab6133a2f318403c01893614a8
Rloundy <rlrjloundy@gmail.com>
2023-07-07 20:41:41 -0500

    Finished Survivor Demo

--- Scripts/SpawnGrunt.cs
+++ Scripts/SpawnGrunt.cs
@@ -4,25 +4,25 @@ namespace GMTKGameJam2023.Scripts;
 
 public partial class SpawnGrunt : Node3D
 {
-    private PackedScene gruntscene;
+	private PackedScene gruntscene;
 
-    private void PlaceGrunt(Vector3 Location)
-    {
-        gruntscene = GD.Load<PackedScene>("res://grunt.tscn");
-        var newgrunt = gruntscene.Instantiate<Grunt>();
-        newgrunt.Position = Location;
-        AddChild(newgrunt);
-    }
+	private void PlaceGrunt(Vector3 Location)
+	{
+		gruntscene = GD.Load<PackedScene>("res://grunt.tscn");
+		var newgrunt = gruntscene.Instantiate<Grunt>();
+		newgrunt.Position = Location;
+		AddChild(newgrunt);
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        base._Input(@event);
-        if (@event is InputEventMouseButton mouseButtonEvent)
-            if (mouseButtonEvent.ButtonIndex == MouseButton.Right && mouseButtonEvent.Pressed)
-            {
-                var camera = GetTree().Root.GetCamera3D();
-                var intersection = camera.CastRay(GetWorld3D().DirectSpaceState, mouseButtonEvent.Position);
-                PlaceGrunt(intersection["position"].AsVector3());
-            }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+		if (@event is InputEventMouseButton mouseButtonEvent)
+			if (mouseButtonEvent.ButtonIndex == MouseButton.Right && mouseButtonEvent.Pressed)
+			{
+				var camera = GetTree().Root.GetCamera3D();
+				var intersection = camera.CastRay(GetWorld3D().DirectSpaceState, mouseButtonEvent.Position);
+				PlaceGrunt(intersection["position"].AsVector3());
+			}
+	}
 }
